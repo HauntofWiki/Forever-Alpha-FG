5290d933958116da9645826e4e09cbea28830bdd
Richard Dobie <39932986+doobifier@users.noreply.github.com>
2020-01-28 21:47:53 -0600

    initial hit detection

---
+++ Assets/Scripts/GamePlayScripts/HitStunManager.cs
@@ -0,0 +1,35 @@
+﻿using GamePlayScripts.CharacterMoves;
+using UnityEngine;
+
+namespace GamePlayScripts
+{
+    public class HitStunManager
+    {
+        private Animator _animator;
+        private CharacterProperties _properties;
+
+        private int _counter = 0;
+
+        public HitStunManager(Animator animator, ref CharacterProperties properties)
+        {
+            _animator = animator;
+            _properties = properties;
+        }
+        
+        public void Update()
+        {
+            if (_counter == 0)
+            {
+                _animator.Play("HitStun");
+                
+            }
+
+            if (_counter > 0 && _counter <= _properties.HitStunDuration)
+            {
+                
+            }
+
+            _counter++;
+        }
+    }
+}
