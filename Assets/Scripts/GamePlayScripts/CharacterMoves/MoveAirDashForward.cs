2104e6c2d2369388d5478921b6a80e86d66a751f
Richard Dobie <39932986+doobifier@users.noreply.github.com>
2020-02-02 20:58:34 -0600

    Refacoring some moves

--- Assets/Scripts/GamePlayScripts/CharacterMoves/MoveAirDashForward.cs
+++ Assets/Scripts/GamePlayScripts/CharacterMoves/MoveAirDashForward.cs
@@ -6,39 +6,40 @@ namespace GamePlayScripts.CharacterMoves
     {
         private CharacterProperties _properties;
         private Animator _animator;
+        private FrameDataHandler _frameData;
         private int _inputLimit;
         private readonly int[] _movePattern = {1,0,1}; //Dash uses X-axis only
         private readonly bool[] _patternMatch = {false, false, false};
         private int _lastInput;
         private int _moveDetectCounter;
 
-        //Tracks invincibility States per frame.
-        //0:None, 1:Full, 2:UpperBody, 3:LowerBody 4:throw
-        public int[] InvincibilyFrames =
-        {
-            0
-        };
-        
-        //Tracks attack properties of the move per frame.
-        //0:StartUp, 1:Active, 2:Recovery 3:End
-        public int[] AttackStateFrames =
-        {
-            0,0,0,1,1,1,1,1,1,1,1,1,1,1,2,2,2,3
-        };
-        
-        //Tracks Cancellable states of the move per frame.
-        //0:None, 1:EmptyCancel, 2:NormalCancel, 3:SpecialCancel, 4:SuperCancel
-        public int[] Cancellability =
-        {
-            1
-        };
-        
-        //Tracks Airborne State of move per frame
-        //0: No, 1:Yes
-        public int[] AirborneState =
-        {
-            1
-        };
+//        //Tracks invincibility States per frame.
+//        //0:None, 1:Full, 2:UpperBody, 3:LowerBody 4:throw
+//        public int[] InvincibilyFrames =
+//        {
+//            0
+//        };
+//        
+//        //Tracks attack properties of the move per frame.
+//        //0:StartUp, 1:Active, 2:Recovery 3:End
+//        public int[] AttackStateFrames =
+//        {
+//            0,0,0,1,1,1,1,1,1,1,1,1,1,1,2,2,2,3
+//        };
+//        
+//        //Tracks Cancellable states of the move per frame.
+//        //0:None, 1:EmptyCancel, 2:NormalCancel, 3:SpecialCancel, 4:SuperCancel
+//        public int[] Cancellability =
+//        {
+//            1
+//        };
+//        
+//        //Tracks Airborne State of move per frame
+//        //0: No, 1:Yes
+//        public int[] AirborneState =
+//        {
+//            1
+//        };
 
         public override void InitializeMove(ref CharacterProperties properties, Animator animator)
         {
@@ -47,7 +48,12 @@ namespace GamePlayScripts.CharacterMoves
             _lastInput = -1;
             _moveDetectCounter = 0;
             _inputLimit = 15;
-            _properties.AirDashDuration = AttackStateFrames.Length;
+            _frameData = new FrameDataHandler(18);
+            _frameData.SetFieldsZero();
+            _frameData.SetAttackFrames(3, 11);
+            
+            
+           // _properties.AirDashDuration = AttackStateFrames.Length;
         }
 
         public override bool DetectMoveInput(InputClass inputClass)
@@ -55,20 +61,28 @@ namespace GamePlayScripts.CharacterMoves
             if (!_properties.IsAirborne) return false;
             _moveDetectCounter++;
         
+            //Check the limit of how many frames are available to input the move
             if (_moveDetectCounter >= _inputLimit)
                 ResetInputDetect();
         
+            //Unity repeats Axis inputs for several frames, this allows us to not check those
             if (_lastInput == inputClass.DPadNumPad)
                 return false;
+            
+            //Check for first forward input
             if (inputClass.DPadX == _movePattern[0] && !_patternMatch[0])
             {
                 _moveDetectCounter = 0;
                 _patternMatch[0] = true;            
             }
+            //Checks for a neutral input
             if (inputClass.DPadX == _movePattern[1] && _patternMatch[0])
                 _patternMatch[1] = true;
+            
+            //Checks for the second forward input
             if (inputClass.DPadX == _movePattern[2] && _patternMatch[1])
                 _patternMatch[2] = true;
+            //Return a positive match
             if (_patternMatch[0] && _patternMatch[1] && _patternMatch[2])
             {
                 ResetInputDetect();
@@ -88,6 +102,7 @@ namespace GamePlayScripts.CharacterMoves
             _patternMatch[2] = false;
         }
 
+        //No holdable effect for AirDash
         public override bool DetectHoldInput(InputClass inputClass)
         {
             return false;
@@ -96,24 +111,27 @@ namespace GamePlayScripts.CharacterMoves
         public override void PerformAction(InputClass inputClass)
         {
             //Play out AirDash Duration
-		if (_properties.CurrentState == CharacterProperties.CharacterState.AirDash && _properties.DashFrameCounter < _properties.AirDashDuration - 1)
-        {
-            _properties.LastState = _properties.CurrentState;
-            _properties.DashFrameCounter++;
-            if (AttackStateFrames[_properties.DashFrameCounter] == 0)
-                _properties.MoveDirection = new Vector3(_properties.AirDashForwardSpeed[0],0,0);
-			if (AttackStateFrames[_properties.DashFrameCounter] == 1)
-                _properties.MoveDirection = new Vector3(_properties.AirDashForwardSpeed[1],0,0);
-            }
-        
-            //Exit dash animation
-            if (_properties.CurrentState == CharacterProperties.CharacterState.AirDash && _properties.DashFrameCounter >= _properties.AirDashDuration - 1)
+		    if (_properties.CurrentState == CharacterProperties.CharacterState.AirDash)
             {
                 _properties.LastState = _properties.CurrentState;
-                _properties.CurrentState = CharacterProperties.CharacterState.JumpForward;
-                _properties.DashFrameCounter = 0;
-                _properties.IsIgnoringGravity = false;
+                _properties.DashFrameCounter++;
+                if (_frameData.AttackFrameState[_properties.DashFrameCounter] == FrameDataHandler.AttackFrameStates.Startup)
+                    _properties.MoveDirection = new Vector3(_properties.AirDashForwardSpeed[0],0,0);
+			    if (_frameData.AttackFrameState[_properties.DashFrameCounter] == FrameDataHandler.AttackFrameStates.Active)
+                    _properties.MoveDirection = new Vector3(_properties.AirDashForwardSpeed[1],0,0);
+                if (_frameData.AttackFrameState[_properties.DashFrameCounter] == FrameDataHandler.AttackFrameStates.Recovery)
+                {
+                    //Exit the dash state
+                    _properties.LastState = _properties.CurrentState;
+                    _properties.CurrentState = CharacterProperties.CharacterState.JumpForward;
+                    _properties.DashFrameCounter = 0;
+                    _properties.IsIgnoringGravity = false;
+                }
+                
+                _frameData.Update(_properties.AttackFrameCounter);
             }
+            
+            
             
             //Begin AirDash Detection
             if (!DetectMoveInput(inputClass)) return;
@@ -125,7 +143,5 @@ namespace GamePlayScripts.CharacterMoves
             _properties.IsIgnoringGravity = true;
             _properties.MoveDirection = new Vector3(_properties.AirDashForwardSpeed[0],0,0);
         }
-
-
     }
 }
--- Assets/Scripts/GamePlayScripts/CharacterMoves/MoveJumpBackward.cs
+++ Assets/Scripts/GamePlayScripts/CharacterMoves/MoveJumpBackward.cs
@@ -1,45 +1,51 @@
 ﻿using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
 
 namespace GamePlayScripts.CharacterMoves
 {
-    
     public class MoveJumpBackward : CharacterMove
     {
         private CharacterProperties _properties;
         private Animator _animator;
-        
-        //Tracks invincibility States per frame.
-        //0:None, 1:Full, 2:UpperBody, 3:LowerBody 4:throw
-        public int[] InvincibilyFrames =
-        {
-            4,4,4,0,0,0
-        };
-        
-        //Tracks attack properties of the move per frame.
-        //0:StartUp, 1:Active, 2:Recovery 3:End
-        public int[] AttackStateFrames =
-        {
-            0,0,0,1,2,2,2,3
-        };
-        
-        //Tracks Cancellable states of the move per frame.
-        //0:None, 1:EmptyCancel, 2:NormalCancel, 3:SpecialCancel, 4:SuperCancel
-        public int[] Cancellability =
-        {
-            0
-        };
-        
-        //Tracks Airborne State of move per frame
-        //0: No, 1:Yes
-        public int[] AirborneState =
-        {
-            0,0,0,1,0,0,0
-        };
+        private FrameDataHandler _frameData;
+
+//        //Tracks invincibility States per frame.
+//        //0:None, 1:Full, 2:UpperBody, 3:LowerBody 4:throw
+//        public int[] InvincibilyFrames =
+//        {
+//            4,4,4,0,0,0
+//        };
+//        
+//        //Tracks attack properties of the move per frame.
+//        //0:StartUp, 1:Active, 2:Recovery 3:End
+//        public int[] AttackStateFrames =
+//        {
+//            0,0,0,1,2,2,2,3
+//        };
+//        
+//        //Tracks Cancellable states of the move per frame.
+//        //0:None, 1:EmptyCancel, 2:NormalCancel, 3:SpecialCancel, 4:SuperCancel
+//        public int[] Cancellability =
+//        {
+//            0
+//        };
+//        
+//        //Tracks Airborne State of move per frame
+//        //0: No, 1:Yes
+//        public int[] AirborneState =
+//        {
+//            0,0,0,1,0,0,0
+//        };
 
         public override void InitializeMove(ref CharacterProperties properties, Animator animator)
         {
             _animator = animator;
             _properties = properties;
+            _frameData = new FrameDataHandler(7);
+            _frameData.SetFieldsZero();
+            //For jumps length isnt important to how they function
+            _frameData.SetAttackFrames(3, 1);
+            _frameData.SetAirborneFrames(0, 6);
         }
 
         public override bool DetectMoveInput(InputClass inputClass)
@@ -53,54 +59,68 @@ namespace GamePlayScripts.CharacterMoves
         }
 
         public override void PerformAction(InputClass inputClass)
-{
-        //Use Character controller to determine if animation have reached the ground
-        if (_properties.CurrentState == CharacterProperties.CharacterState.JumpBackward && _properties.CharacterController.isGrounded && AttackStateFrames[_properties.JumpFrameCounter] != 0)
         {
-            _properties.JumpFrameCounter++;
-            _properties.LastState = _properties.CurrentState;
-            _properties.CurrentState = CharacterProperties.CharacterState.LandingJumpBackward; 
-        }
-            
-        //Advance Jump Frame Counter and assess Startup/Recovery
-        if (_properties.CurrentState == CharacterProperties.CharacterState.JumpBackward && AttackStateFrames[_properties.JumpFrameCounter] == 0)
-        {
-            _properties.JumpFrameCounter++;
-            if (AttackStateFrames[_properties.JumpFrameCounter] == 0)
-                return;
-            if (AttackStateFrames[_properties.JumpFrameCounter] == 1)
+            Debug.Log(_properties.CurrentState + ", " + _frameData.AttackFrameState[_properties.JumpFrameCounter]);
+            //Use Character controller to determine if animation has reached the ground
+            if (_properties.CurrentState == CharacterProperties.CharacterState.JumpBackward &&
+                _properties.CharacterController.isGrounded &
+                _frameData.AttackFrameState[_properties.JumpFrameCounter] != FrameDataHandler.AttackFrameStates.Startup)
             {
-                _properties.IsAirborne = true;
-                _properties.MoveDirection = new Vector3(-_properties.WalkBackwardXSpeed, _properties.JumpYSpeed, 0);
-            }
-        }
-        
-        if (_properties.CurrentState == CharacterProperties.CharacterState.LandingJumpBackward)
-        {
-            _properties.JumpFrameCounter++;
-            if (AttackStateFrames[_properties.JumpFrameCounter] == 3)
-            {
-                _properties.JumpFrameCounter = 0;
+                _properties.JumpFrameCounter++;
                 _properties.LastState = _properties.CurrentState;
-                _properties.IsAirborne = false;
-                _properties.CurrentState = CharacterProperties.CharacterState.Stand;
+                _properties.CurrentState = CharacterProperties.CharacterState.LandingJumpBackward;
+                _frameData.Update(_properties.JumpFrameCounter);
             }
-        }
 
-        //Detect proper state and detect input
-        if (_properties.CurrentState == CharacterProperties.CharacterState.JumpBackward && AttackStateFrames[_properties.JumpFrameCounter] == 1)
-        {
-            _properties.MoveDirection.x = -_properties.WalkBackwardXSpeed;
-            return;
-        }
+            //Advance Jump Frame Counter and assess Startup/Recovery
+            if (_properties.CurrentState == CharacterProperties.CharacterState.JumpBackward)
+            {
+                if (_frameData.AttackFrameState[_properties.JumpFrameCounter] ==
+                    FrameDataHandler.AttackFrameStates.Startup)
+                {
+                    _properties.JumpFrameCounter++;
+                    return;
+                }
 
-        if (!DetectMoveInput(inputClass)) return;
-        if (_properties.CurrentState != CharacterProperties.CharacterState.Crouch && _properties.CurrentState != CharacterProperties.CharacterState.Stand && _properties.CurrentState != CharacterProperties.CharacterState.CancellableAnimation) return;
-        
-        _properties.JumpFrameCounter = 0;
-        _properties.LastState = _properties.CurrentState;
-        _properties.CurrentState = CharacterProperties.CharacterState.JumpBackward;
-    }
-        
+                if (_frameData.AttackFrameState[_properties.JumpFrameCounter] ==
+                    FrameDataHandler.AttackFrameStates.Active)
+                {
+                    _properties.IsAirborne = true;
+                    _properties.MoveDirection = new Vector3(-_properties.WalkBackwardXSpeed, _properties.JumpYSpeed, 0);
+                }
+
+                _frameData.Update(_properties.JumpFrameCounter);
+            }
+
+            if (_properties.CurrentState == CharacterProperties.CharacterState.LandingJumpBackward)
+            {
+                _properties.JumpFrameCounter++;
+                if (_frameData.AttackFrameState[_properties.JumpFrameCounter] !=
+                    FrameDataHandler.AttackFrameStates.Recovery)
+                {
+                    _properties.JumpFrameCounter = 0;
+                    _properties.LastState = _properties.CurrentState;
+                    _properties.IsAirborne = false;
+                    _properties.CurrentState = CharacterProperties.CharacterState.Stand;
+                }
+            }
+
+            //Detect proper state and detect input
+            if (_properties.CurrentState == CharacterProperties.CharacterState.JumpBackward &&
+                _frameData.AttackFrameState[_properties.JumpFrameCounter] != FrameDataHandler.AttackFrameStates.Active)
+            {
+                _properties.MoveDirection.x = -_properties.WalkBackwardXSpeed;
+                return;
+            }
+
+            if (!DetectMoveInput(inputClass)) return;
+            if (_properties.CurrentState != CharacterProperties.CharacterState.Crouch &&
+                _properties.CurrentState != CharacterProperties.CharacterState.Stand && _properties.CurrentState !=
+                CharacterProperties.CharacterState.CancellableAnimation) return;
+
+            _properties.JumpFrameCounter = 0;
+            _properties.LastState = _properties.CurrentState;
+            _properties.CurrentState = CharacterProperties.CharacterState.JumpBackward;
+        }
     }
 }
